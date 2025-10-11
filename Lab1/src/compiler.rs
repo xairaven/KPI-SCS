cc7285072756fa4b1e1d5adb50beada142f0897b
xairaven <alexkovalevkpi@gmail.com>
2025-10-11 12:02:46 +0300

    (Lab 1) Feat: Implement syntax error reporting

--- Lab1/src/compiler.rs
+++ Lab1/src/compiler.rs
@@ -1,12 +1,37 @@
-use crate::compiler::syntax::SyntaxAnalyzer;
+use crate::compiler::syntax::{SyntaxAnalyzer, SyntaxError};
+use colored::Colorize;
 
 pub fn compile(source: &str) -> String {
     let tokens = tokenizer::tokenize(source);
     let syntax_errors = SyntaxAnalyzer::new(tokens).analyze();
-    dbg!(syntax_errors);
 
-    // TODO: Implement lexical analysis and syntax analysis here
-    source.to_owned()
+    report(source, &syntax_errors)
+}
+
+fn report(source: &str, syntax_errors: &[SyntaxError]) -> String {
+    let mut result = String::new();
+
+    let first_line = match syntax_errors.len() {
+        0 => format!("{}: {}\n", "Analysis result".bold(), "OK!".bold().green()),
+        n => {
+            format!(
+                "{}: Found {} {}.\n",
+                "Analysis result".bold(),
+                n.to_string().red(),
+                "errors".red()
+            )
+        },
+    };
+    result.push_str(&first_line);
+
+    result.push_str(&format!("\n{}:\n", "Code".bold().yellow()));
+    result.push_str(source);
+
+    for error in syntax_errors {
+        // TODO: formatting
+    }
+
+    result
 }
 
 pub mod syntax;
