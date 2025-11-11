c2d409f4bee54006eb2c8807279b56570357c92b
xairaven <alexkovalevkpi@gmail.com>
2025-11-11 10:40:08 +0200

    (Lab 3-4) Feat: Refactor equivalent forms search to separate expansion and factoring stages

--- Lab3-4/src/compiler/ast/equivalent_forms.rs
+++ Lab3-4/src/compiler/ast/equivalent_forms.rs
@@ -7,55 +7,66 @@ use std::collections::{HashSet, VecDeque};
 
 impl AbstractSyntaxTree {
     pub fn find_equivalent_forms(&self) -> Vec<AbstractSyntaxTree> {
-        // BFS
-        let mut queue: VecDeque<AbstractSyntaxTree> = VecDeque::new();
-        let mut visited: HashSet<String> = HashSet::new();
         let mut all_forms: Vec<AbstractSyntaxTree> = Vec::new();
+        let mut visited: HashSet<String> = HashSet::new();
+        let mut expansion_queue: VecDeque<AbstractSyntaxTree> = VecDeque::new();
+
+        // --- Stage 1: Expansion (Distributive Law, Nodes 0-7) ---
+        // This stage finds all forms *only* by expanding parentheses.
 
         let initial_key = self.to_canonical_string();
-
-        queue.push_back(self.clone());
+        expansion_queue.push_back(self.clone());
         visited.insert(initial_key);
         all_forms.push(self.clone());
 
-        // A limit to avoid infinite loops if something goes wrong
-        let mut iterations_limit = 5000;
+        let mut fully_expanded_node: Option<AbstractSyntaxTree> = None;
 
-        // Running the search for equivalent forms
-        while let Some(current_ast) = queue.pop_front() {
-            if iterations_limit <= 0 {
-                log::error!("Iteration limit reached! Aborting search.");
-                break;
-            }
-            iterations_limit -= 1;
-
-            // --- 1. Distributive law (Opening brackets) ---
-            // Calling a new "non-greedy" function
+        while let Some(current_ast) = expansion_queue.pop_front() {
             let expansion_steps = current_ast.get_all_single_step_expansions();
+
+            if expansion_steps.is_empty() && fully_expanded_node.is_none() {
+                fully_expanded_node = Some(current_ast.clone());
+            }
 
             for expanded_ast in expansion_steps {
                 let key = expanded_ast.to_canonical_string();
-
-                // Is this form unique?
                 if !visited.contains(&key) {
-                    visited.insert(key);
+                    visited.insert(key.clone());
                     all_forms.push(expanded_ast.clone());
-                    queue.push_back(expanded_ast);
+                    expansion_queue.push_back(expanded_ast);
                 }
             }
+        }
 
-            // --- 2. Associative Law (Parentheses) ---
-            // Calling a new "non-greedy" function
+        // --- Stage 2: Factoring (Associative Law, Nodes 7-13) ---
+        // This stage starts *only* with the fully expanded form
+        // and *only* applies factoring (collapse).
+
+        let Some(start_node_for_factoring) = fully_expanded_node else {
+            // This can happen if there was nothing to expand in the original expression.
+            // In example `(a-c)*k...` this node will 100% be found.
+            // If it is not there, we simply return the forms from Step 1.
+            log::warn!(
+                "No fully expanded form found (Node 7). Skipping Stage 2 (Factoring)."
+            );
+            return all_forms;
+        };
+
+        let mut factoring_queue: VecDeque<AbstractSyntaxTree> = VecDeque::new();
+        // We don't need to add `start_node_for_factoring` to `all_forms` or `visited` again,
+        // since it's already there from Step 1.
+        factoring_queue.push_back(start_node_for_factoring);
+
+        while let Some(current_ast) = factoring_queue.pop_front() {
             let factoring_steps = current_ast.get_all_single_step_factorings();
 
             for factored_ast in factoring_steps {
                 let key = factored_ast.to_canonical_string();
-
-                // Is this form unique?
                 if !visited.contains(&key) {
-                    visited.insert(key);
+                    // We are continuing to build on the same `visited` set
+                    visited.insert(key.clone());
                     all_forms.push(factored_ast.clone());
-                    queue.push_back(factored_ast);
+                    factoring_queue.push_back(factored_ast);
                 }
             }
         }
